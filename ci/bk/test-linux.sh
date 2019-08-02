31dd848b4ae8cee2666b82ac7ccd46322a2d0165
Jared Hefty <jared@improbable.io>
2019-08-02 10:21:19 +0100

    Add badges and fix up CI

--- ci/bk/test-linux.sh
+++ ci/bk/test-linux.sh
@@ -9,4 +9,3 @@ source "ci/pinned-tools.sh"
 startDockerCompose ./ci/docker/docker-compose.yml
 
 dc exec -T dotnet /bin/bash -c "./ci/test.sh"
-dc exec -T dotnet /bin/bash -c "./scripts/init-database.sh && ./scripts/create-snapshot.sh"
