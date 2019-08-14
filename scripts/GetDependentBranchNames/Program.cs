ffa97d42496028f261025e83fa885d33bf4c4e7d
Jared Hefty <jared@improbable.io>
2019-08-14 13:37:38 +0100

    Reflect improvements from csharp branch

--- scripts/GetDependentBranchNames/Program.cs
+++ scripts/GetDependentBranchNames/Program.cs
@@ -1,83 +1,70 @@
 using System;
-using System.Diagnostics;
-using System.IO;
-using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Medallion.Shell;
 
 namespace BuildNugetPackages
 {
+
     internal class Program
     {
+        private static readonly Shell shell = new Shell(options => options.ThrowOnError());
+
+        private struct Dependency
+        {
+            public string Label { get; set; }
+            public string Repo { get; set; }
+            public string Trigger { get; set; }
+        }
+
+        private static readonly List<Dependency> Dependencies = new List<Dependency>
+        {
+            new Dependency
+            {
+                Label = "build-database-sync-worker-example",
+                Trigger = "database-sync-worker-example-premerge",
+                Repo = "git@github.com:improbable/database_sync_worker_example.git"
+            }
+        };
+
         private static int Main(string[] args)
         {
             try
             {
-                // Default to "master", unless the downstream dependency has a branch matching the same name, then use that.
                 var currentBranch = Environment.GetEnvironmentVariable("BUILDKITE_BRANCH");
-                var checkRemoteBranch = RunRedirected("git", "ls-remote", "--heads", "git@github.com:improbable/database_sync_worker_example.git", currentBranch).Trim();
 
-                var remoteBranch = checkRemoteBranch.Contains(currentBranch) ? currentBranch : "master";
+                if (string.IsNullOrEmpty(currentBranch))
+                {
+                    throw new Exception("The BUILDKITE_BRANCH environment variable is empty or not set.");
+                }
 
-                Console.Out.WriteLine($@"steps:
-  - label: ""build-database-sync-worker-example""
-    trigger: database-sync-worker-example-premerge
+                foreach (var dep in Dependencies)
+                {
+                    var lines = new List<string>();
+                    shell.Run("git", "ls-remote", "--heads", dep.Repo, currentBranch)
+                        .RedirectTo(lines)
+                        .RedirectStandardErrorTo(Console.Error)
+                        .Wait();
+
+                    var remoteBranch = lines.Any() && lines.First().Contains(currentBranch) ? currentBranch : "master";
+
+                    Console.Out.WriteLine($@"steps:
+  - label: ""{dep.Label}""
+    trigger: {dep.Trigger}
     build:
       branch: {remoteBranch}
       env:
         DBSYNC_WORKER_BRANCH: ""{currentBranch}""
         CSHARP_TEMPLATE_BRANCH: ""$CSHARP_TEMPLATE_BRANCH""");
+                }
             }
+
             catch (Exception e)
             {
                 Console.Error.WriteLine(e.Message);
                 return 1;
             }
             return 0;
-        }
-
-        private static string RunRedirected(string command, params string[] args)
-        {
-            var info = new ProcessStartInfo(command, string.Join(' ', args))
-            {
-                CreateNoWindow = true,
-                UseShellExecute = false,
-                RedirectStandardError = true,
-                RedirectStandardOutput = true
-            };
-
-            using (var process = Process.Start(info))
-            {
-                if (process == null) throw new Exception($"Failed to start {command}");
-
-                var output = new StringBuilder();
-                process.OutputDataReceived += (sender, eventArgs) =>
-                {
-                    if (!string.IsNullOrEmpty(eventArgs.Data))
-                    {
-                        output.AppendLine(eventArgs.Data);
-                    }
-                };
-
-                process.ErrorDataReceived += (sender, eventArgs) =>
-                {
-                    if (!string.IsNullOrEmpty(eventArgs.Data))
-                    {
-                        Console.Error.WriteLine(eventArgs.Data);
-                    }
-                };
-
-                process.EnableRaisingEvents = true;
-                process.BeginOutputReadLine();
-                process.BeginErrorReadLine();
-
-                process.WaitForExit();
-
-                if (process.ExitCode != 0)
-                {
-                    throw new Exception($"Non-zero exit code: {command} {string.Join(" ", args)}");
-                }
-
-                return output.ToString();
-            }
         }
     }
 }
