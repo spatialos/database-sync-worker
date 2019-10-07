f539c5d71d534165ef137700ba300640e2f3c1e7
Jared Dixey-Hefty <jared@improbable.io>
2019-10-07 17:06:09 +0100

    Teardown docker Remove separate build step

--- ci/test.sh
+++ ci/test.sh
@@ -7,3 +7,6 @@ cd "$(dirname "$0")/../"
 ./scripts/build-nuget-packages.sh
 
 dotnet build ./Workers.sln -p:Platform=x64
+./scripts/publish-windows-workers.sh
+./scripts/publish-osx-workers.sh
+./scripts/publish-linux-workers.sh
