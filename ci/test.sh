1aded3e85c092e33db74e297f124ffb33fe519fc
Jared Dixey-Hefty <jared@improbable.io>
2019-10-08 09:08:27 +0100

    Squash dotnet welcome message Cleanup logs

--- ci/test.sh
+++ ci/test.sh
@@ -4,9 +4,17 @@ set -euxo pipefail
 
 cd "$(dirname "$0")/../"
 
+echo "--- Build NuGet packges"
 ./scripts/build-nuget-packages.sh
 
+echo "--- Build solution"
 dotnet build ./Workers.sln -p:Platform=x64
+
+echo "--- Publish Windows workers"
 ./scripts/publish-windows-workers.sh
+
+echo "--- Publish OSX workers"
 ./scripts/publish-osx-workers.sh
+
+echo "--- Publish Linux workers"
 ./scripts/publish-linux-workers.sh
